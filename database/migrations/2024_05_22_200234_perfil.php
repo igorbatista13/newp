61bce862b8bc6fbfa0faee0958917f1f65b36eeb
igorbatista13 <igorbatista13@gmail.com>
2024-05-22 16:25:06 -0400

    migration perfil

--- database/migrations/2024_05_22_200234_perfil.php
+++ database/migrations/2024_05_22_200234_perfil.php
@@ -13,7 +13,32 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('perfil', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id'); // Chave estrangeira para a tabela de usuários
+
+            //     $table->unsignedBigInteger('user_id');
+            //  $table->string('Nome_completo')->nullable();
+            $table->string('Nome')->nullable();
+            $table->string('Sobrenome')->nullable();
+            $table->string('Sexo')->nullable();
+            $table->date('Data_Nascimento')->nullable();
+            $table->string('Email')->nullable();
+            $table->string('Estado')->nullable();
+            $table->string('Cidade')->nullable();
+            $table->string('CEP')->nullable();
+            $table->string('Telefone')->nullable();
+            $table->string('Facebook')->nullable();
+            $table->string('Instagram')->nullable();
+            $table->string('Linkedin')->nullable();
+            $table->string('Site')->nullable();
+            $table->string('image')->nullable();
+            $table->string('Tipo')->nullable();
+
+            $table->timestamps();
+
+            //  $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
     }
 
     /**
@@ -23,6 +48,6 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('perfil');
     }
 };
